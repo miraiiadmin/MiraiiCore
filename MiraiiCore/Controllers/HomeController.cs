0ee0ccf4259bafe847c1a4afedfb620daf7175a9
nattawat-wib <nattawat.wib@gmail.com>
2020-07-10 18:21:54 +0700

    Edit : - font - carousel & text on review page - move all image to floder "image"
    Add :
    - about page
    - about button
    
    Fail :
    - full footer

--- MiraiiCore/Controllers/HomeController.cs
+++ MiraiiCore/Controllers/HomeController.cs
@@ -27,8 +27,11 @@ namespace MiraiiCore.Controllers
         {
             return View();
         }
-
-       
+    
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
