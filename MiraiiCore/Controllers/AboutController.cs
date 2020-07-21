cf8a27d4b6338b96dee2ac7396534db5878ec777
nattawat-wib <nattawat.wib@gmail.com>
2020-07-21 18:46:20 +0700

    Connect about page to database edit : home button color -  modal design - John wick bug - search button

--- MiraiiCore/Controllers/AboutController.cs
+++ MiraiiCore/Controllers/AboutController.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MiraiiCore.Models;
 
 namespace MiraiiCore.Controllers
 {
     public class AboutController : Controller
     {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
         public IActionResult Index()
         {
             return View();
         }
+
+        void ConnectionString()
+        {
+            con.ConnectionString = "Data Source=DESKTOP-27AKM7H\\MSSQLSERVER01;Initial Catalog=Miraii;Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+
+        [HttpPost]
+        public IActionResult Feedback(FeedbackModel insertFeedback) //Insert Feedback Action
+        {
+            DateTime date = DateTime.Now;
+            ConnectionString();
+            con.Open();
+
+            com.Connection = con;
+            com.CommandText = "INSERT INTO Feedback(Feedback,Date) VALUES (@Feedback,@Date)";
+
+            com.Parameters.AddWithValue("@Feedback", insertFeedback.Feedback);
+            com.Parameters.AddWithValue("@Date", date.ToString());
+            com.ExecuteNonQuery();
+            con.Close();
+
+            return RedirectToAction("Index", "About");
+
+        }
+
     }
 }
