d909664caa5d162af9e943c37cb6cb77fe221215
nattawat-wib <nattawat.wib@gmail.com>
2020-07-26 19:01:07 +0700

    add : AdminController - AdminViewModel

--- MiraiiCore/Controllers/AdminController.cs
+++ MiraiiCore/Controllers/AdminController.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MiraiiCore.Models;
+using System.Data.SqlClient;
 
 namespace MiraiiCore.Controllers
 {
     public class AdminController : Controller
     {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source=DESKTOP-27AKM7H\\MSSQLSERVER01; database=Miraii; integrated security = SSPI;";
+        }
+
+        [HttpPost]
+        public IActionResult Verify(Admin acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from AdminLogin where username='"+acc.username+"' and password ='"+acc.password+"'";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View("FileUploader");
+            }
+            else
+            {
+                con.Close();
+                return View("Index");
+            }
         }
 
         public IActionResult FileUploader()
---
+++ MiraiiCore/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiraiiCore.Models
+{
+    public class Admin
+    {
+ 
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
