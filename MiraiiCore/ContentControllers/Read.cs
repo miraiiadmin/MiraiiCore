a1bff07bb30a37eceaa63e7ff64bc946c7f7986d
nattawat-wib <nattawat.wib@gmail.com>
2020-08-05 21:01:51 +0700

    - edit controller - font test

---
+++ MiraiiCore/ContentControllers/Read.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiraiiCore.Controllers
+{
+    public class Read : Controller
+    {
+        public IActionResult DrRomactic()
+        {
+            return View();
+        }
+
+
+    }
+}
