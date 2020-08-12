ee00ef6e741f06167a77fd3a5dc1c35800a48ae4
nattawat-wib <nattawat.wib@gmail.com>
2020-08-12 13:18:35 +0700

    Edit : Content page design - Title name - connect DB on all content controller - manage css code - add icon on all title bar
    Remove : Carousel controller

--- MiraiiCore/ContentControllers/Review.cs
+++ MiraiiCore/ContentControllers/Review.cs
@@ -1,23 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MiraiiCore.Models;
 
 namespace MiraiiCore.Controllers
 {
     public class Review : Controller
     {
-        public IActionResult Index()
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        public IActionResult Index(ContentDataViewModel data)
+
         {
-            return View("~/Views/Read/Review/Index.cshtml");
-        }
+            ConnectionString();
+            con.Open();
+            com.Connection = con;
 
-        public IActionResult DrRomantic()
+            com.CommandText = "SELECT TOP 9 * FROM ContentData ORDER BY ContentID DESC";
+            dr = com.ExecuteReader();
+            List<ContentDataViewModel> objmodel = new List<ContentDataViewModel>();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    var details = new ContentDataViewModel();
+                    details.ContentLocation = dr["ContentLocation"].ToString();
+                    details.ContentName = dr["ContentName"].ToString();
+                    details.ContentCategory = dr["ContentCategory"].ToString();
+                    details.ContentDescription = dr["ContentDescription"].ToString();
+                    details.ContentDate = dr["ContentDate"].ToString();
+                    details.ContentWriter = dr["ContentWriter"].ToString();
+                    details.Controller = dr["Controller"].ToString();
+                    details.Action = dr["Action"].ToString();
+                    objmodel.Add(details);
+                }
+                data.ContentInfo = objmodel;
+            }
+            else
+            {
+                return View("Error");
+            }
+
+            con.Close();
+            ModelState.Clear();
+            return View("~/Views/Read/Review/Index.cshtml", data);
+        }
+        void ConnectionString()
         {
-            return View("~/Views/Read/Review/DrRomantic.cshtml");
+            con.ConnectionString = "Data Source= miraii.space; Database= miraii_space; User ID=miraii_space; Password=Hostmiraii007;";
         }
-
-
     }
 }
