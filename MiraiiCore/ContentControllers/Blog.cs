854dade1be97957f1898d6e712696a48e495df86
nattawat-wib <nattawat.wib@gmail.com>
2020-08-18 18:59:41 +0700

    add file Article pattern add view "SevenSkillsForFreshGraduate "

--- MiraiiCore/ContentControllers/Blog.cs
+++ MiraiiCore/ContentControllers/Blog.cs
@@ -57,6 +57,12 @@ namespace MiraiiCore.Controllers
             con.ConnectionString = "Data Source= miraii.space; Database= miraii_space; User ID=miraii_space; Password=Hostmiraii007;";
         }
 
+        
+        public IActionResult SevenSkillsForFreshGraduate()
+        {
+            return View("~/Views/Read/Blog/SevenSkillsForFreshGraduate.cshtml");
+        }
+
         public IActionResult ProjectPowerNetflix()
         {
             return View("~/Views/Read/Blog/ProjectPowerNetflix.cshtml");
