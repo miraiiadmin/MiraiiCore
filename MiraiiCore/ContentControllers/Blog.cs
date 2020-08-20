de86878d8569c6f623ab8422da38857db95a11cf
nattawat-wib <nattawat.wib@gmail.com>
2020-08-20 23:07:02 +0700

    - prepare article page for tenet

--- MiraiiCore/ContentControllers/Blog.cs
+++ MiraiiCore/ContentControllers/Blog.cs
@@ -56,8 +56,11 @@ namespace MiraiiCore.Controllers
         {
             con.ConnectionString = "Data Source= miraii.space; Database= miraii_space; User ID=miraii_space; Password=Hostmiraii007;";
         }
+        public IActionResult Tenet()
+        {
+            return View("~/Views/Read/Blog/Tenet.cshtml");
+        }
 
-        
         public IActionResult SevenSkillsForFreshGraduate()
         {
             return View("~/Views/Read/Blog/SevenSkillsForFreshGraduate.cshtml");
